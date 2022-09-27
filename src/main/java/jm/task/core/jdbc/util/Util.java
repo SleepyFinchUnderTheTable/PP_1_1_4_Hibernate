d469ebbd99e8e7b3c49adf9690b66460dc408f04
SleepyFinchUnderTheTable <setos1996@gmail.com>
2022-09-27 20:07:32 +0300

    samovar v.2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,7 +2,9 @@ package jm.task.core.jdbc.util;
 
 import org.hibernate.cfg.Configuration;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class Util {
